e7d5e712cae624d4bcf626dcea0b4cc38b2b233a
akemys <lukacsisz92@gmail.com>
2019-06-12 09:31:30 +0200

    new option: text

--- Loading.js
+++ Loading.js
@@ -2,137 +2,148 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { View, TouchableWithoutFeedback, ActivityIndicator, StyleSheet, Dimensions } from 'react-native'
+import {View, TouchableWithoutFeedback, ActivityIndicator, StyleSheet, Dimensions, Text} from 'react-native'
 import RootSiblings from 'react-native-root-siblings'
-import { Bubbles, DoubleBounce, Bars, Pulse } from 'react-native-loader'
+import {Bubbles, DoubleBounce, Bars, Pulse} from 'react-native-loader'
 
-const { height, width } = Dimensions.get('window')
+const {height, width} = Dimensions.get('window')
 
 const LoadingType = {
-  'Bubbles': Bubbles,
-  'DoubleBounce': DoubleBounce,
-  'Bars': Bars,
-  'Pulse': Pulse,
-  'Spinner': ActivityIndicator
+    'Bubbles': Bubbles,
+    'DoubleBounce': DoubleBounce,
+    'Bars': Bars,
+    'Pulse': Pulse,
+    'Spinner': ActivityIndicator
 }
 
 class LoadingContainer extends React.Component {
-  static propTypes = {
-    closeOnTouch: PropTypes.bool,
-    color: PropTypes.string,
-    size: PropTypes.number,
-    overlayColor: PropTypes.string,
-    loadingType: PropTypes.oneOf(['Bubbles', 'DoubleBounce', 'Bars', 'Pulse', 'Spinner'])
-  }
-
-  static defaultProps = {
-    color: '#FFFFFF',
-    size: 20,
-    overlayColor: 'rgba(0,0,0,0.5)',
-    closeOnTouch: false,
-    loadingType: 'Bars'
-  }
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      visible: this.props.visible
+    static propTypes = {
+        closeOnTouch: PropTypes.bool,
+        color: PropTypes.string,
+        text: PropTypes.string,
+        size: PropTypes.number,
+        overlayColor: PropTypes.string,
+        loadingType: PropTypes.oneOf(['Bubbles', 'DoubleBounce', 'Bars', 'Pulse', 'Spinner'])
     }
-  }
 
-  componentDidMount = () => {
-    if (this.state.visible) {
-      this.show()
+    static defaultProps = {
+        color: '#FFFFFF',
+        size: 20,
+        text: null,
+        overlayColor: 'rgba(0,0,0,0.5)',
+        closeOnTouch: false,
+        loadingType: 'Bars'
     }
-  }
 
-  componentWillUnmount = () => {
-    this.close()
-  }
-
-  shouldComponentUpdate = (nextProps, nextState) => {
-    return this.state.visible !== nextState.visible
-  }
-
-  show () {
-    this.setState({ visible: true })
-  }
-
-  close () {
-    this.setState({ visible: false })
-    this.props.onClose && this.props.onClose()
-  }
-
-  render () {
-    let { overlayColor, loadingType, size, color } = this.props
-    if (loadingType === 'Spinner') {
-      size = 'large'
+    constructor(props) {
+        super(props)
+        this.state = {
+            visible: this.props.visible
+        }
     }
-    const Loading = LoadingType[loadingType]
-    return this.state.visible ? (
-      <View style={[styles.modalWrapper, { backgroundColor: overlayColor }]}>
-        <TouchableWithoutFeedback onPress={() => this.props.closeOnTouch === true ? this.setState({ visible: false }) : null}>
-          <View style={styles.modalUnderlay} />
-        </TouchableWithoutFeedback>
-        <View style={styles.content}>
-          <Loading size={size} color={color} />
-        </View>
-      </View>
-    ) : null
-  }
+
+    componentDidMount = () => {
+        if (this.state.visible) {
+            this.show()
+        }
+    }
+
+    componentWillUnmount = () => {
+        this.close()
+    }
+
+    shouldComponentUpdate = (nextProps, nextState) => {
+        return this.state.visible !== nextState.visible
+    }
+
+    show() {
+        this.setState({visible: true})
+    }
+
+    close() {
+        this.setState({visible: false})
+        this.props.onClose && this.props.onClose()
+    }
+
+    render() {
+        let {overlayColor, loadingType, size, color, text} = this.props
+        if (loadingType === 'Spinner') {
+            size = 'large'
+        }
+        const Loading = LoadingType[loadingType]
+        return this.state.visible ? (
+            <View style={[styles.modalWrapper, {backgroundColor: overlayColor}]}>
+                <TouchableWithoutFeedback
+                    onPress={() => this.props.closeOnTouch === true ? this.setState({visible: false}) : null}>
+                    <View style={styles.modalUnderlay}/>
+                </TouchableWithoutFeedback>
+                <View style={styles.content}>
+                    {text ? <Text style={styles.text}>
+                        {text}
+                    </Text> : null}
+                    <Loading size={size} color={color}/>
+                </View>
+            </View>
+        ) : null
+    }
 }
 
 class Loading extends React.Component {
-  static displayName = 'Loading';
-  static propTypes = LoadingContainer.propTypes;
+    static displayName = 'Loading';
+    static propTypes = LoadingContainer.propTypes;
 
-  static show = (options = {}) => {
-    return new RootSiblings(<LoadingContainer
-      {...options}
-      visible
-    />)
-  };
+    static show = (options = {}) => {
+        return new RootSiblings(<LoadingContainer
+            {...options}
+            visible
+        />)
+    };
 
-  static hide = loading => {
-    if (loading instanceof RootSiblings) {
-      loading.destroy()
-    } else {
-      console.warn(`Loading.hide expected a \`RootSiblings\` instance as argument.\nBut got \`${typeof alert}\` instead.`)
+    static hide = loading => {
+        if (loading instanceof RootSiblings) {
+            loading.destroy()
+        } else {
+            console.warn(`Loading.hide expected a \`RootSiblings\` instance as argument.\nBut got \`${typeof alert}\` instead.`)
+        }
+    };
+
+    render() {
+        return null
     }
-  };
-
-  render () {
-    return null
-  }
 }
 
 const styles = StyleSheet.create({
-  modalWrapper: {
-    flex: 1,
-    position: 'absolute',
-    width,
-    height,
-    left: 0,
-    top: 0,
-    backgroundColor: '#000',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  modalUnderlay: {
-    position: 'absolute',
-    width,
-    height,
-    left: 0,
-    top: 0,
-    bottom: 0,
-    right: 0,
-    backgroundColor: 'transparent'
-  },
-  content: {}
+    modalWrapper: {
+        flex: 1,
+        position: 'absolute',
+        width,
+        height,
+        left: 0,
+        top: 0,
+        backgroundColor: '#000',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    modalUnderlay: {
+        position: 'absolute',
+        width,
+        height,
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+        backgroundColor: 'transparent'
+    },
+    text: {
+      color: 'white',
+      fontSize: 20,
+      marginBottom: 30
+    },
+    content: {}
 })
 
 export {
-  RootSiblings as Manager
+    RootSiblings as Manager
 }
 export default Loading
